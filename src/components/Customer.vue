10ac268a92eb31edc0ee81c030805952a027e14a
Gary Sentosa <gary.sentosa@zuuonline.com>
2023-06-19 12:58:57 +0900

    feat: step 2

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -2,6 +2,7 @@
   <div class="customer">
     <h1>Customerです</h1>
     <p>{{ totalTvCount }} テレビがあります</p>
+    <button :disabled="!totalTvCount" @click="buyTv">Buy TV</button>
   </div>
 </template>
 
@@ -11,6 +12,11 @@ export default {
   computed: {
     totalTvCount() {
       return this.$store.state.totalTvCount;
+    },
+  },
+  methods: {
+    buyTv() {
+      this.$store.dispatch("removeTv");
     },
   },
 };
