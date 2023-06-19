d35f6a68eeb02db83138f80a2f82bd08a804ab7e
Gary Sentosa <gary.sentosa@zuuonline.com>
2023-06-19 12:40:42 +0900

    feat: step 1

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import Vuex from "vuex";
+import Vue from "vue";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    totalTvCount: 10,
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+});
