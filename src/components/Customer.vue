6cd2fb948289f4ff09208dfe6c5bd81e61279eca
Gary Sentosa <gary.sentosa@zuuonline.com>
2023-06-19 12:40:42 +0900

    feat: step 1

---
+++ src/components/Customer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="customer">
+    <h1>Customerです</h1>
+    <p>{{ totalTvCount }} テレビがあります</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomerComponent",
+  computed: {
+    totalTvCount() {
+      return this.$store.state.totalTvCount;
+    },
+  },
+};
+</script>
