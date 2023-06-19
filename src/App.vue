f204e616a4440bb9d3a4bd484fc4c103329e48b4
Gary Sentosa <gary.sentosa@zuuonline.com>
2023-06-19 12:40:42 +0900

    feat: step 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Customer />
+  </div>
 </template>
 
 <script>
+import Customer from "./components/Customer";
+
 export default {
   name: "App",
-  components: {},
+  components: { Customer },
 };
 </script>
